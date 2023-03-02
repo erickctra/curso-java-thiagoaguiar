7822a7e2248eae907a8be49adf328f45ac2592f3
Erick Cintra <developer.erickcintra@outlook.com>
2023-03-01 22:19:02 -0300

    aula 6: build errors

---
+++ aula - 6/FirstProject/src/FirstProgram.java
@@ -0,0 +1,14 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        // 2 tipos de erros
+        // erro de compilaçao
+
+        String number1 = "olamundo";
+
+        // erro de execução
+        int x = 21;
+        int y = 0;
+        System.out.println(x / y);
+    }
+
+}
