464a9fcc0a30e80fbbc75bdec570f244f0ccc7ff
Erick Cintra <developer.erickcintra@outlook.com>
2023-03-01 22:08:34 -0300

    aula 5: variables

---
+++ aula - 5/FirstProject/src/FirstProgram.java
@@ -0,0 +1,15 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+       var login = "user";
+       var password = "1234";
+
+        // variaveis -> tipo: String
+        // variaveis -> tipo: Numbers
+        // variaveis -> tipo: Double
+        // variaveis -> tipo: booleans
+
+        System.out.println(login);
+        System.out.println(password);
+    }
+
+}
