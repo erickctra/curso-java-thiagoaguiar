23a21aeccc9da5a787023899f165c7d5a2042ee2
Erick Cintra <developer.erickcintra@outlook.com>
2023-03-01 21:50:36 -0300

    aula 4: entry points

---
+++ aula - 4/FirstProject/src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        printCall("Hello World");
+    }
+
+    static void printCall(String args) {
+        System.out.println(args);
+    }
+}
