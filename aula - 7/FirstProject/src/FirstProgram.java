5b2de566d6877103d0850f588be029ead571d013
Erick Cintra <developer.erickcintra@outlook.com>
2023-03-02 22:17:11 -0300

    aula 7: functions

---
+++ aula - 7/FirstProject/src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        sum(10, 20);
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+}
